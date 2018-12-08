3020b59f15529793762271f0f0fd6dc9d25150e0
Carolina Herbster <carolhmj@gmail.com>
2018-12-08 18:06:32 -0300

    solution

--- day3/code.py
+++ day3/code.py
@@ -1,27 +1,44 @@
 import re
-import intervaltree
+from intervaltree import IntervalTree
 
 def read_input():
 	with open('input.txt') as f:
 		content = f.read()
-	return content
+
+	return content.split('\n')
 
 def process_content_into_intervals(content):
-	x_intervals = IntervalTree()
-	y_intervals = IntervalTree()
+	x_interval = IntervalTree()
+	y_interval = IntervalTree()
 	for line in content:
-		m = re.search('(\d{1,4}),(\d{1,4}): (\d{1,4})x(\d{1,4})', line)
+		m = re.search('#(\d{1,4}) @ (\d{1,4}),(\d{1,4}): (\d{1,4})x(\d{1,4})', line)
 		
-		x_i = int(m.group(1))
-		y_i = int(m.group(2))
-		x_f = x_i + int(m.group(3))
-		y_f = y_i + int(m.group(4))
+		if m:
+			x_i = int(m.group(2))
+			y_i = int(m.group(3))
+			x_f = x_i + int(m.group(4))
+			y_f = y_i + int(m.group(5))
 		
-		x_interval[x_i:x_f] = 1
-		y_interval[y_i:y_f] = 1
+			x_interval[x_i:x_f] = m.group(1)
+			y_interval[y_i:y_f] = m.group(1)
 
 	return (x_interval, y_interval)	
 
+def find_overlapping_area(x_interval, y_interval):
+	area = 0
+	for x in range(0,999):
+		allgroupsinx = {i.data for i in x_interval[x]}
+		# print (allgroupsinx)
+		for y in range(0,999):
+			allgroupsiny = {i.data for i in y_interval[y]}
+			inter = allgroupsinx & allgroupsiny
+			if len(inter) > 1:
+				area = area + 1
+		
+	return area
+					
 if __name__ == '__main__':
 	content = read_input()
-	process_content_into_intervals(content)		
+	(x_interval, y_interval) = process_content_into_intervals(content)
+	area = find_overlapping_area(x_interval, y_interval)
+	print(area)
